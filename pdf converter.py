a84bcbf9d44446bb5c9b5c6a07f5a841b73c9f2a
likhithamuppuru <muppurulikhitha12@gmail.com>
2024-05-30 19:26:51 +0530

    Add files via upload

---
+++ pdf converter.py
@@ -0,0 +1,28 @@
+import PyPDF2
+
+def pdf_to_text(pdf_path, output_txt):
+    # Open the PDF file in read-binary mode
+    with open(pdf_path, 'rb') as pdf_file:
+        # Create a PdfReader object instead of PdfFileReader
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # Initialize an empty string to store the text
+        text = ''
+
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+
+    # Write the extracted text to a text file
+    with open(output_txt, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(text)
+
+if __name__ == "__main__":
+    pdf_path=input("Enter the Pdf Path ")
+    output_txt = input("Enter the Txt Path")
+
+     
+
+    pdf_to_text(pdf_path, output_txt)
+
+    print("PDF converted to text successfully!")
